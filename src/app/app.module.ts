472e76e3e7ed4d5008d89f59459e012c1af9f79e
tiffanycabernathy96 <tiffanycabernathy96@gmail.com>
2018-04-24 21:04:30 -0400

    commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { Network } from '@ionic-native/network';
 import { Geolocation } from '@ionic-native/geolocation';
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { AgmCoreModule } from '@agm/core';
+import { Facebook } from '@ionic-native/facebook';
 
 @NgModule({
   declarations: [
@@ -85,6 +86,7 @@ import { AgmCoreModule } from '@agm/core';
 	GoogleMapsProvider,
 	Network,
 	Geolocation,
+	Facebook,
 	HttpClientModule,
 	{provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
